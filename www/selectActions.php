c3c17fa697fca8973e4108fb0bf7e56f7ba31f5f
Quejao <leotrone@hotmail.com>
2018-11-12 20:42:31 -0200

    Changes on files to padronize the access to the data base

--- www/selectActions.php
+++ www/selectActions.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'database.php';
+require_once('database.php');
 $db = new MyDB();
 if (!$db) {
     echo $db->lastErrorMsg();
--- www/selectMissions.php
+++ www/selectMissions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'database.php';
+require_once('database.php');
 
 $db = new MyDB();
 if (!$db) {
